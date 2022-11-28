6389f279c9a80b61548925bd131821f987ce1ad7
SirPopper <48968500+SirPopper@users.noreply.github.com>
2022-11-28 17:08:47 +0900

    worked on drawing and input
    1. added drawing logic 2. added input text fields for grid 3. added color change. All without prompts.

--- game.js
+++ game.js
@@ -6,25 +6,53 @@ function makeRows(rows, cols) {
   container.style.setProperty("--grid-cols", cols);
   for (let i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
-    cell.innerText = i + 1;
     container.appendChild(cell).className = "grid-item";
     cell.setAttribute("id", `grid-${i}`);
   }
 }
 
-makeRows(16, 16);
+makeRows(0, 0);
 
-const isHover = (e) => e.parentElement.querySelector(":hover") === e;
+//input fields
+const inputOne = document.querySelector("#first-input");
+const inputTwo = document.querySelector("#second-input");
 
-const cells = document.querySelectorAll(".grid-item");
-cells.forEach((cell) => {
-  cell.style.color = "red";
-  cell.addEventListener("mousemove", function checkHover() {
-    const hovered = isHover(cell);
-    if (hovered !== checkHover.hovered) {
-      console.log(hovered ? "hovered" : "not hovered");
-      checkHover.hovered = hovered;
-      cell.style.background = "black";
-    }
+//take input color
+const colorInput = document.querySelector("#color-input");
+
+//pressedSubmit function in addEventListener
+function pressedSubmit() {
+  deleteGrid();
+  makeRows(inputOne.value, inputTwo.value);
+  drawing();
+  console.log(colorInput.value);
+}
+
+const submitBtn = document.querySelector(".btn-submit");
+submitBtn.addEventListener("click", pressedSubmit);
+
+//drawing with hover
+function drawing() {
+  //checks if mouse hovered over event
+  const isHover = (e) => e.parentElement.querySelector(":hover") === e;
+
+  const cells = document.querySelectorAll(".grid-item"); //selects all cells
+  cells.forEach((cell) => {
+    cell.style.color = "red";
+    cell.addEventListener("mousemove", function checkHover() {
+      const hovered = isHover(cell);
+      if (hovered !== checkHover.hovered) {
+        console.log(hovered ? "hovered" : "not hovered");
+        checkHover.hovered = hovered;
+        cell.style.background = `${colorInput.value}`;
+        console.log(colorInput.value);
+      }
+    });
   });
-});
+}
+
+//delete grid-items
+function deleteGrid() {
+  const grid = document.querySelectorAll(".grid-item");
+  grid.forEach((grid) => container.removeChild(grid));
+}
